a31ef583f66d702643cb2f2bb6e82a0e03b57dc8
cjolalla <carlosgosu@gmail.com>
2019-04-13 12:58:20 +0200

    Primer commit

---
+++ src/test/java/es/cjolalla/catalogingredients/CatalogIngredientsApplicationTests.java
@@ -0,0 +1,16 @@
+package es.cjolalla.catalogingredients;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CatalogIngredientsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
